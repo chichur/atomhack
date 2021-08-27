0529f785e08345197ee58118ec54f6b73c6ca07f
chichur <chichur@physicon.ru>
2021-08-27 23:26:31 +0300

    тестовое дерево организации

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import StyledTreeExample from './components/orgSctructTree/orgSctructTree'
 
 function App() {
   return (
     <div className="App">
-
+      <StyledTreeExample/>
     </div>
   );
 }
---
+++ src/components/orgSctructTree/orgNode.js
@@ -0,0 +1,12 @@
+// здесь я пытался вставить компонент как узел дерева
+// но безререзультатно
+
+const orgNode = () => {
+    return (
+        <div className="orgNode">
+            <p>Kek</p>
+        </div>
+    );
+}
+
+export default orgNode;
